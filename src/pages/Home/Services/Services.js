084837d28c89b4d79044e7f95e64af91a6c3c271
shohid986 <shohidul9863371@gmail.com>
2021-11-01 23:51:03 +0600

    added home services spinners

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Spinner } from 'react-bootstrap';
+import useAuth from '../../../Hooks/useAuth';
 import Service from '../Service/Service';
 
 const Services = () => {
+    const {isLoading} = useAuth();
     const [services, setServices] = useState([]);
     useEffect(()=>{
         fetch('https://gruesome-witch-85056.herokuapp.com/services')
         .then(res => res.json())
         .then(data => setServices(data))
     }, [])
+    if(isLoading){
+        return <Spinner animation="border" variant="primary" />
+    }
     return (
         <div className="container my-5 py-5">
             <div className="mx-auto mb-4 text-center">
